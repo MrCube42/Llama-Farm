401b2fa93ba7a3cefc0fc6f955ab3dae558eb457
David Würfel <david.wuerfel@googlemail.com>
2020-08-15 13:19:05 +0200

    feat: rename to Llama, first view

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Llamas />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from "vue"
+import Llamas from "./components/Llamas.vue"
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
-  }
-});
+    Llamas,
+  },
+})
 </script>
 
 <style lang="scss">
---
+++ src/components/Llamas.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li v-for="llama in llamas" :key="llama.id">{{ llama.name }}</li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+interface Llama {
+  id: number
+  name: string
+}
+
+export default Vue.extend({
+  data() {
+    return {
+      llamas: [
+        {
+          id: 1,
+          name: "D'Artagnan",
+        },
+        {
+          id: 1,
+          name: "Athos",
+        },
+        {
+          id: 1,
+          name: "Porthos",
+        },
+        {
+          id: 1,
+          name: "Aramis",
+        },
+      ] as Llama[],
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
