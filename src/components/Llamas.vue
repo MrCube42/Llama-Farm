bcb44d482babd5432c46b79038e9a6bb2cf69c25
David Würfel <david.wuerfel@googlemail.com>
2020-08-16 15:49:49 +0200

    chore: extract search into setup

--- src/components/Llamas.vue
+++ src/components/Llamas.vue
@@ -41,12 +41,41 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue"
-
 import { Llama } from "./Llama"
 import { fetchLlamas } from "./LlamasService"
 
-export default Vue.extend({
+import {
+  defineComponent,
+  Ref,
+  ref,
+  onMounted,
+  computed,
+} from "@vue/composition-api"
+
+export default defineComponent({
+  setup() {
+    const llamas: Ref<Llama[]> = ref([])
+    const searchText = ref("")
+
+    onMounted(async () => {
+      llamas.value = await fetchLlamas()
+    })
+
+    const filteredLlamas = computed(() => {
+      if (searchText.value !== "") {
+        return llamas.value.filter(llama =>
+          llama.name.toLowerCase().includes(searchText.value.toLowerCase()),
+        )
+      } else {
+        return llamas.value
+      }
+    })
+
+    return {
+      searchText,
+      filteredLlamas,
+    }
+  },
   data() {
     return {
       llamas: [] as Llama[],
